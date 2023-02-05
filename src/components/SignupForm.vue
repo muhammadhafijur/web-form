fad33f93630db0b5a490ffb04daf9c2d18fdd6c7
muhammadhafijur <hafijur.salah@gmail.com>
2023-02-05 15:18:14 +0600

    :sparkles: feat: Submitting forms

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,9 +1,10 @@
 <template>
-  <form>
+  <form @submit.prevent="handleSubmit">
     <label>Email:</label>
     <input type="email" required v-model="email" />
     <label>Password:</label>
     <input type="password" required v-model="password" />
+    <div v-if="passwordError" class="error"> {{ passwordError }} </div>
 
     <label>Role:</label>
     <select v-model="role">
@@ -21,6 +22,11 @@
       <input type="checkbox" v-model="terms" required />
       <label>Accept terms and conditions</label>
     </div>
+
+    <div class="submit">
+        <button>Create an Account</button>
+    </div>
+
   </form>
   <p>Email: {{ email }}</p>
   <p>Password: {{ password }}</p>
@@ -38,6 +44,7 @@ export default {
       terms: false,
       tempSkill: "",
       skills: [],
+      passwordError: ''
     };
   },
   methods: {
@@ -53,6 +60,18 @@ export default {
         this.skills = this.skills.filter((item) => {
             return skill !== item
         })
+    },
+    handleSubmit(){
+        // validate password
+        this.passwordError = this.password.length > 5 ? '' : 'Password must be at least 6 chars long'
+
+        if(!this.passwordError){
+            console.log('email: ', this.email)
+            console.log('password: ', this.password)
+            console.log('role: ', this.role)
+            console.log('skills: ', this.skills)
+            console.log('terms accepted: ', this.terms)
+        }
     }
   },
 
@@ -105,5 +124,22 @@ input[type="checkbox"] {
     font-weight: bold;
     color: #777;
     cursor: pointer;
+}
+button{
+    background: #0b6dff;
+    border: 0;
+    padding: 10px 20px;
+    margin-top: 20px;
+    color: white;
+    border-radius: 20px;
+}
+.submit{
+    text-align: center;
+}
+.error{
+    color: #ff0062;
+    margin-top: 10px;
+    font-size: 0.8em;
+    font-weight: bold;
 }
 </style>
